a1a58d01aa554131fb29abda7c665673a1817269
SEUIL <sone91961@gmail.com>
2024-11-18 19:05:45 +0900

    로그인 및 회원가입 시스템 기능 추가 완료

--- TimeToDo/Forms/LoginForm1.cs
+++ TimeToDo/Forms/LoginForm1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
@@ -7,40 +8,18 @@ namespace TimeToDo
 {
     public partial class LoginForm1 : Form
     {
-        private int SelectedRowIndex;
-        private string connectionString = "User Id=test01; Password=clrchr91961; " +
-            "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1522))(CONNECT_DATA=(SERVICE_NAME=xe)))";
-
-        OracleDataAdapter DBAdapter; // Data Provider인 DBAdapter 입니다. 
-        DataSet DS; // DataSet 객체입니다. 
-        OracleCommandBuilder myCommandBuilder;
+        private DBClass db;
 
         public LoginForm1()
         {
             InitializeComponent();
-            DB_Open();
-            TestDBConnection();
+            db = new DBClass();  // DBClass 객체 초기화
         }
 
         private void ClearTextBoxes()
         {
             txtUserId.Clear();
             txtPassword.Clear();
-        }
-
-        private void DB_Open()
-        {
-            try
-            {
-                string commandString = "SELECT * FROM UserTable";
-                DBAdapter = new OracleDataAdapter(commandString, connectionString);
-                myCommandBuilder = new OracleCommandBuilder(DBAdapter);
-                DS = new DataSet();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("DB 연결 오류: " + ex.Message);
-            }
         }
 
         private void summit_loginPage_Click(object sender, EventArgs e)
@@ -71,64 +50,101 @@ namespace TimeToDo
         private bool ValidateUser(string userId, string password)
         {
             bool isValidUser = false;
+            /*if (userId.Equals("1234") && password.Equals("1234"))
+            {
+                return isValidUser=true;
+            }*/
 
             try
             {
-                // 올바른 테이블 및 컬럼 이름 사용
-                string query = "SELECT COUNT(*) FROM USERTABLE WHERE USERID = :userId AND PASSWORD = :password";
+                // 매개변수를 사용한 쿼리
+                string query = "SELECT COUNT(*) FROM Users WHERE USERID = :userId AND PASSWORD = :password";
 
-                using (OracleConnection connection = new OracleConnection(connectionString))
-                using (OracleCommand command = new OracleCommand(query, connection))
+                // 매개변수를 딕셔너리로 설정
+                var parameters = new Dictionary<string, object>
                 {
-                    command.Parameters.Add(new OracleParameter("userId", userId));
-                    command.Parameters.Add(new OracleParameter("password", password));
+                    { "userId", userId },
+                    { "password", password }
+                };
 
-                    connection.Open();
-                    object result = command.ExecuteScalar();
+                // DBClass의 DB_Open 메서드를 호출하여 DataSet을 반환받음
+                DataSet userDataSet = db.DB_Open(query, parameters);
 
-                    if (result != null && Convert.ToInt32(result) > 0)
-                    {
-                        isValidUser = true;
-                    }
+                // 결과가 존재하는지 확인 (COUNT(*) 결과가 1 이상이면 해당 사용자가 존재)
+                if (userDataSet.Tables[0].Rows.Count > 0 && Convert.ToInt32(userDataSet.Tables[0].Rows[0][0]) > 0)
+                {
+                    isValidUser = true;
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("오류 발생: " + ex.Message);
+                MessageBox.Show("LoginForm1 오류 발생: " + ex.Message);
+            }
+            finally
+            {
+                db.DB_Close(); // 연결을 닫습니다.
             }
 
             return isValidUser;
         }
 
-        private void TestDBConnection()
+        private void signUp_loginPage_Click(object sender, EventArgs e)
         {
-            string connectionString = "User Id=test01; Password=clrchr91961; Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1522))(CONNECT_DATA=(SERVICE_NAME=xe)))";
-
-            using (OracleConnection connection = new OracleConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
-                    if (connection.State == ConnectionState.Open)
-                    {
-                        MessageBox.Show("데이터베이스 연결 성공!");
-                    }
-                    else
-                    {
-                        MessageBox.Show("데이터베이스 연결 실패!");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("연결 오류: " + ex.Message);
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
+            // SignUpForm1으로 이동
+            SignUpForm1 signUpForm = new SignUpForm1();
+            signUpForm.Show();
+            this.Hide(); // LoginForm1을 숨김
         }
-
-
     }
 }
+
+
+/*private bool ValidateUser(string userId, string password)
+{
+bool isValidUser = false;
+if (userId.Equals("1234") && password.Equals("1234"))
+{
+    isValidUser = true;
+    return isValidUser;
+} //계속 오류나서 일단 로그인 하드코딩
+
+try
+{
+    // 대소문자를 무시하고 비교하기 위해 UPPER() 함수 사용
+    string query = "SELECT * FROM Users WHERE USERID = :userId AND PASSWORD = :password";
+
+    using (OracleCommand command = new OracleCommand(query, db.Connection))
+    {
+        // 매개변수 설정
+        command.Parameters.Add("userId", OracleDbType.Varchar2, 50).Value = userId;
+        command.Parameters.Add("password", OracleDbType.Varchar2, 50).Value = password;
+
+        // 연결이 닫혀 있는 경우에만 엽니다.
+        if (db.Connection.State == ConnectionState.Closed)
+        {
+            db.Connection.Open();
+        }
+
+        using (OracleDataReader reader = command.ExecuteReader())
+        {
+            // 결과가 있으면 해당 사용자 정보가 존재한다는 뜻
+            if (reader.Read())
+            {
+                isValidUser = true;
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    MessageBox.Show("LoginForm1 오류 발생: " + ex.Message);
+}
+finally
+{
+    if (db.Connection.State == ConnectionState.Open)
+    {
+        db.Connection.Close();
+    }
+}
+return isValidUser;
+}*/
