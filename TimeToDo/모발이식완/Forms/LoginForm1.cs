4e1ec654f38229058b528bbb1ad70fa9242b6789
kimdiyong <kimmmmminseo@gmail.com>
2024-11-22 02:29:22 +0900

    추가폼, 검색폼 하는중

---
+++ TimeToDo/모발이식완/Forms/LoginForm1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace TimeToDo
+{
+    public partial class LoginForm1 : Form
+    {
+        public static class Session //로그인시 FK 전달용 세션
+        {
+            public static String LoggedInUserId { get; set; } // 로그인한 사용자 ID를 저장
+        }
+
+        private DBClass db;
+
+        public LoginForm1()
+        {
+            InitializeComponent();
+            db = new DBClass();  // DBClass 객체 초기화
+        }
+
+        private void ClearTextBoxes()
+        {
+            txtUserId.Clear();
+            txtPassword.Clear();
+        }
+
+        private void summit_loginPage_Click(object sender, EventArgs e)
+        {
+            // 아이디와 비밀번호 가져오기
+            string userId = txtUserId.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("아이디와 비밀번호를 입력해주세요.");
+                return;
+            }
+
+            // 로그인 검증 함수 호출
+            if (ValidateUser(userId, password)) //로그인 성공
+            {
+                MessageBox.Show("로그인 성공!");
+                // 로그인 성공 시 다음 화면으로 이동하거나 필요한 작업 수행
+
+                Session.LoggedInUserId = userId; // 로그인한 사용자 ID 저장
+
+                // calendarForm으로 이동
+                calenderForm1 calenderForm1 = new calenderForm1();
+                calenderForm1.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("아이디 또는 비밀번호가 잘못되었습니다.");
+                ClearTextBoxes();
+            }
+        }
+
+        private bool ValidateUser(string userId, string password)
+        {
+            bool isValidUser = false;
+            /*if (userId.Equals("1234") && password.Equals("1234"))
+            {
+                return isValidUser=true;
+            }*/
+
+            try
+            {
+                // 매개변수를 사용한 쿼리
+                string query = "SELECT COUNT(*) FROM Users WHERE USERID = :userId AND PASSWORD = :password";
+
+                // 매개변수를 딕셔너리로 설정
+                var parameters = new Dictionary<string, object>
+                {
+                    { "userId", userId },
+                    { "password", password }
+                };
+
+                // DBClass의 DB_Open 메서드를 호출하여 DataSet을 반환받음
+                DataSet userDataSet = db.DB_Open(query, parameters);
+
+                // 결과가 존재하는지 확인 (COUNT(*) 결과가 1 이상이면 해당 사용자가 존재)
+                if (userDataSet.Tables[0].Rows.Count > 0 && Convert.ToInt32(userDataSet.Tables[0].Rows[0][0]) > 0)
+                {
+                    isValidUser = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("LoginForm1 오류 발생: " + ex.Message);
+            }
+            finally
+            {
+                db.DB_Close(); // 연결을 닫습니다.
+            }
+
+            return isValidUser;
+        }
+
+        private void signUp_loginPage_Click(object sender, EventArgs e)
+        {
+            // SignUpForm1으로 이동
+            SignUpForm1 signUpForm = new SignUpForm1();
+            signUpForm.Show();
+            this.Hide(); // LoginForm1을 숨김
+        }
+    }
+}
+
+
---
+++ TimeToDo/모발이식완/Forms/calenderForm1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace TimeToDo
+{
+    partial class calenderForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.category = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.descriptionEvent = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.repeats = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.eventCalendar = new System.Windows.Forms.Label();
+            this.addEvent = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calendar
+            // 
+            this.calendar.CalendarDimensions = new System.Drawing.Size(3, 1);
+            this.calendar.Font = new System.Drawing.Font("굴림", 9F);
+            this.calendar.Location = new System.Drawing.Point(18, 38);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(315, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "달력";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.category,
+            this.time,
+            this.descriptionEvent,
+            this.repeats});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 269);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(671, 227);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // category
+            // 
+            this.category.Text = "카테고리";
+            this.category.Width = 76;
+            // 
+            // time
+            // 
+            this.time.Text = "시간";
+            this.time.Width = 99;
+            // 
+            // descriptionEvent
+            // 
+            this.descriptionEvent.Text = "일정 상세";
+            this.descriptionEvent.Width = 193;
+            // 
+            // repeats
+            // 
+            this.repeats.Text = "일정 반복";
+            this.repeats.Width = 86;
+            // 
+            // eventCalendar
+            // 
+            this.eventCalendar.AutoSize = true;
+            this.eventCalendar.Font = new System.Drawing.Font("굴림", 15F);
+            this.eventCalendar.Location = new System.Drawing.Point(315, 246);
+            this.eventCalendar.Name = "eventCalendar";
+            this.eventCalendar.Size = new System.Drawing.Size(49, 20);
+            this.eventCalendar.TabIndex = 3;
+            this.eventCalendar.Text = "일정";
+            // 
+            // addEvent
+            // 
+            this.addEvent.Location = new System.Drawing.Point(15, 502);
+            this.addEvent.Name = "addEvent";
+            this.addEvent.Size = new System.Drawing.Size(81, 37);
+            this.addEvent.TabIndex = 4;
+            this.addEvent.Text = "일정 추가";
+            this.addEvent.UseVisualStyleBackColor = true;
+            this.addEvent.Click += new System.EventHandler(this.addEvent_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "일정 편집";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 502);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "일정 삭제";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(276, 502);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(81, 37);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "반복 설정";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(18, 212);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(78, 34);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "일정 검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnExit.Location = new System.Drawing.Point(581, 502);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(105, 37);
+            this.BtnExit.TabIndex = 11;
+            this.BtnExit.Text = "프로그램 종료";
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(609, 234);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(77, 32);
+            this.btnRefresh.TabIndex = 12;
+            this.btnRefresh.Text = "새로고침";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // calenderForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 556);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addEvent);
+            this.Controls.Add(this.eventCalendar);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calendar);
+            this.Name = "calenderForm1";
+            this.Text = "calenderForm1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label eventCalendar;
+        private System.Windows.Forms.ColumnHeader time;
+        private System.Windows.Forms.ColumnHeader descriptionEvent;
+        private System.Windows.Forms.ColumnHeader repeats;
+        private System.Windows.Forms.Button addEvent;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColumnHeader category;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
